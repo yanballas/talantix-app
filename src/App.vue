7d119293f69ab5f78b652449011f3625b41cd8b9
v.serbin <yanballas@yandex.ru>
2024-07-02 10:30:06 +0300

    feat init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="app">
+    <HelloWorld msg="Hello world" />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  width: 80vw;
+  height: 80vh;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 48px;
+    color: #fff;
+  }
+</style>
